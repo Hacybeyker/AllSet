45b202d8e0d6f871fb930f8900590a39b5735d69
Carlos Osorio <hacybeyker@gmail.com>
2022-07-07 19:13:49 -0500

    update yml

--- buildSrc/src/main/kotlin/Configuration.kt
+++
@@ -1,5 +0,0 @@
-object Configuration {
-    const val applicationId = "com.hacybeyker.allset"
-    const val versionCode = 6
-    const val versionName = "6.0.0"
-}
---
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -0,0 +1,5 @@
+object ConfigureApp {
+    const val applicationId = "com.hacybeyker.allset"
+    const val versionCode = 7
+    const val versionName = "6.0.1"
+}

4ac1b034d9a7fae0c5497c44a5ac95cfd04cdf39
Carlos Osorio <hacybeyker@gmail.com>
2024-10-31 22:19:53 -0500

    update version 12 - 7.1.3
    Took 3 minutes

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,5 +1,5 @@
 object ConfigureApp {
     const val applicationId = "com.hacybeyker.allset"
-    const val versionCode = 11
-    const val versionName = "7.1.2"
+    const val versionCode = 12
+    const val versionName = "7.1.3"
 }

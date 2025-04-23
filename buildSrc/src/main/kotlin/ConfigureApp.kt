95e515a767fecfd686d595c01f46af0600172f00
Carlos Osorio <hacybeyker@gmail.com>
2025-04-23 11:13:03 -0500

    Update app version code to 15 and version name to 7.3.0.
    Took 1 minute

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,5 +1,5 @@
 object ConfigureApp {
     const val applicationId = "com.hacybeyker.allset"
-    const val versionCode = 14
-    const val versionName = "7.2.0"
+    const val versionCode = 15
+    const val versionName = "7.3.0"
 }
